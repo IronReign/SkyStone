a61d32770788f5cc099e4d82a26fba079b2a6261
CooperJ-0 <48371707+CooperJ-0@users.noreply.github.com>
2020-03-07 11:26:37 -0600

    took out the only STOP_AND_RESET_ENCODER that i could find for turret

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/tombot/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/tombot/Turret.java
@@ -58,7 +58,7 @@ public class Turret{
 
     public Turret(DcMotor motor, BNO055IMU turretIMU) {
 
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor.setTargetPosition(motor.getCurrentPosition());
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         //this.magSensor = magSensor;
